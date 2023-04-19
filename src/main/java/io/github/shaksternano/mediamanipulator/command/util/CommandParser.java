c2423f23a6c02a2d4142ca39bdd0cb33e3d4d6a8
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-04-19 14:02:48 +0100

    perf: commands run asynchronously

--- src/main/java/io/github/shaksternano/mediamanipulator/command/util/CommandParser.java
+++ src/main/java/io/github/shaksternano/mediamanipulator/command/util/CommandParser.java
@@ -17,8 +17,8 @@ import org.jetbrains.annotations.Nullable;
 import java.text.DecimalFormat;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
@@ -35,43 +35,63 @@ public class CommandParser {
      * @param event The {@link MessageReceivedEvent} that triggered the command.
      */
     public static void parseAndExecute(MessageReceivedEvent event) {
-        Message userMessage = event.getMessage();
-        String stringMessage = DiscordUtil.getContentStrippedKeepEmotes(userMessage).trim();
-        List<String> commandParts = parseCommandParts(stringMessage);
+        var triggerMessage = event.getMessage();
+        var stringMessage = DiscordUtil.getContentStrippedKeepEmotes(triggerMessage).trim();
+        var commandParts = parseCommandParts(stringMessage);
 
-        MessageChannel channel = event.getChannel();
+        var channel = event.getChannel();
 
         if (commandParts.size() > 0) {
-            Optional<Command> commandOptional = CommandRegistry.getCommand(commandParts.get(0));
-
-            commandOptional.ifPresent(command -> {
+            CommandRegistry.getCommand(commandParts.get(0)).ifPresent(command -> {
                 try {
                     channel.sendTyping().queue();
-                    List<String> arguments = parseBaseArguments(commandParts, command);
-                    ListMultimap<String, String> extraArguments = parseExtraArguments(commandParts, command);
-
-                    try {
-                        command.execute(arguments, extraArguments, event);
-                    } catch (PermissionException e) {
-                        userMessage.reply("This bot doesn't have the required permissions to execute this command!").queue();
-                        Main.getLogger().error("This bot doesn't have the required permissions needed to execute command " + command.getNameWithPrefix() + "!", e);
-                    } catch (InvalidArgumentException e) {
-                        userMessage.reply(e.getMessage() == null ? "Invalid arguments!" : "Invalid arguments: " + e.getMessage()).queue();
-                    } catch (MissingArgumentException e) {
-                        userMessage.reply(e.getMessage() == null ? "Missing arguments!" : "Missing arguments: " + e.getMessage()).queue();
-                    } catch (OutOfMemoryError e) {
-                        userMessage.reply("The server ran out of memory trying to execute this command! Try again later.").queue();
-                        Main.getLogger().error("Ran out of memory trying to execute command " + command.getNameWithPrefix() + "!", e);
-                    } catch (Throwable t) {
-                        userMessage.reply("Error executing command!").queue();
-                        Main.getLogger().error("Error executing command " + command.getNameWithPrefix() + "!", t);
-                    }
+                    var arguments = parseBaseArguments(commandParts, command);
+                    var extraArguments = parseExtraArguments(commandParts, command);
+                    CompletableFuture.runAsync(() -> {
+                        try {
+                            command.execute(arguments, extraArguments, event);
+                        } catch (Throwable t) {
+                            throw new UncheckedException(t);
+                        }
+                    }).whenComplete((unused, throwable) -> {
+                        if (throwable != null) {
+                            handleError(getCause(throwable), triggerMessage, command);
+                        }
+                    });
                 } catch (PermissionException e) {
                     Main.getLogger().error("Missing send message permission!", e);
                 }
             });
         }
     }
+
+    private static Throwable getCause(Throwable throwable) {
+        if (throwable instanceof UncheckedException) {
+            var cause = throwable.getCause();
+            if (cause != null) {
+                return cause;
+            }
+        }
+        return throwable;
+    }
+
+    private static void handleError(Throwable error, Message userMessage, Command command) {
+        if (error instanceof PermissionException) {
+            userMessage.reply("This bot doesn't have the required permissions to execute this command!").queue();
+            Main.getLogger().error("This bot doesn't have the required permissions needed to execute command " + command.getNameWithPrefix() + "!", error);
+        } else if (error instanceof InvalidArgumentException) {
+            userMessage.reply(error.getMessage() == null ? "Invalid arguments!" : "Invalid arguments: " + error.getMessage()).queue();
+        } else if (error instanceof MissingArgumentException) {
+            userMessage.reply(error.getMessage() == null ? "Missing arguments!" : "Missing arguments: " + error.getMessage()).queue();
+        } else if (error instanceof OutOfMemoryError) {
+            userMessage.reply("The server ran out of memory trying to execute this command! Try again later.").queue();
+            Main.getLogger().error("Ran out of memory trying to execute command " + command.getNameWithPrefix() + "!", error);
+        } else {
+            userMessage.reply("Error executing command!").queue();
+            Main.getLogger().error("Error executing command " + command.getNameWithPrefix() + "!", error);
+        }
+    }
+
 
     /**
      * Splits a message into a string array, splitting on spaces.
@@ -82,12 +102,11 @@ public class CommandParser {
     private static List<String> parseCommandParts(String message) {
         if (message.length() > 1) {
             if (message.startsWith(Command.PREFIX)) {
-                String[] commandParts = message.split("\\s+");
+                var commandParts = message.split("\\s+");
                 commandParts[0] = commandParts[0].substring(1).toLowerCase();
                 return ImmutableList.copyOf(commandParts);
             }
         }
-
         return ImmutableList.of();
     }
 
@@ -98,14 +117,12 @@ public class CommandParser {
      * @return The arguments of the command.
      */
     private static List<String> parseBaseArguments(List<String> commandParts, Command command) {
-        ImmutableList.Builder<String> argumentsBuilder = new ImmutableList.Builder<>();
-
-        boolean passedFirst = false;
-        for (String commandPart : commandParts) {
+        var argumentsBuilder = new ImmutableList.Builder<String>();
+        var passedFirst = false;
+        for (var commandPart : commandParts) {
             if (passedFirst) {
                 if (commandPart.startsWith(Command.PREFIX)) {
-                    String commandWord = commandPart.substring(1).toLowerCase();
-
+                    var commandWord = commandPart.substring(1).toLowerCase();
                     if (command.getAdditionalParameterNames().contains(commandWord)) {
                         break;
                     }
@@ -121,14 +138,12 @@ public class CommandParser {
     }
 
     private static ListMultimap<String, String> parseExtraArguments(List<String> commandParts, Command command) {
-        ImmutableListMultimap.Builder<String, String> argumentsBuilder = new ImmutableListMultimap.Builder<>();
+        var argumentsBuilder = new ImmutableListMultimap.Builder<String, String>();
         Set<String> passedExtraCommandWords = new HashSet<>();
-
         String currentExtraParameterName = null;
-        for (String commandPart : commandParts) {
+        for (var commandPart : commandParts) {
             if (commandPart.startsWith(Command.PREFIX)) {
-                String commandWord = commandPart.substring(1).toLowerCase();
-
+                var commandWord = commandPart.substring(1).toLowerCase();
                 if (command.getAdditionalParameterNames().contains(commandWord)
                     && !passedExtraCommandWords.contains(commandWord)
                 ) {
@@ -137,7 +152,6 @@ public class CommandParser {
                     continue;
                 }
             }
-
             if (currentExtraParameterName != null) {
                 argumentsBuilder.put(currentExtraParameterName, commandPart);
             }
@@ -148,58 +162,56 @@ public class CommandParser {
 
     public static int parseIntegerArgument(List<String> arguments, int toParseIndex, int defaultValue, @Nullable Predicate<Integer> constraint, MessageChannel triggerChannel, BiFunction<String, String, String> errorMessage) {
         if (arguments.size() > toParseIndex) {
-            String argument = arguments.get(toParseIndex);
-
+            var argument = arguments.get(toParseIndex);
             try {
-                int result = Integer.decode(argument);
+                var result = Integer.decode(argument);
                 if (constraint == null || constraint.test(result)) {
                     return result;
                 }
             } catch (NumberFormatException ignored) {
             }
-
             triggerChannel.sendMessage(errorMessage.apply(argument, String.valueOf(defaultValue))).queue();
             triggerChannel.sendTyping().queue();
         }
-
         return defaultValue;
     }
 
     public static float parseFloatArgument(List<String> arguments, int toParseIndex, float defaultValue, @Nullable Predicate<Float> constraint, MessageChannel triggerChannel, BiFunction<String, String, String> errorMessage) {
         if (arguments.size() > toParseIndex) {
-            String argument = arguments.get(toParseIndex);
-
+            var argument = arguments.get(toParseIndex);
             try {
-                float result = Float.parseFloat(argument);
+                var result = Float.parseFloat(argument);
                 if (constraint == null || constraint.test(result)) {
                     return result;
                 }
             } catch (NumberFormatException ignored) {
             }
-
             triggerChannel.sendMessage(errorMessage.apply(argument, FORMAT.format(defaultValue))).queue();
             triggerChannel.sendTyping().queue();
         }
-
         return defaultValue;
     }
 
     public static float parseFloatExtraArgument(ListMultimap<String, String> extraArguments, String parameterName, float defaultValue, @Nullable Predicate<Float> constraint, MessageChannel triggerChannel, BiFunction<String, String, String> errorMessage) {
         if (extraArguments.containsKey(parameterName)) {
-            String argument = extraArguments.get(parameterName).get(0);
-
+            var argument = extraArguments.get(parameterName).get(0);
             try {
-                float result = Float.parseFloat(argument);
+                var result = Float.parseFloat(argument);
                 if (constraint == null || constraint.test(result)) {
                     return Float.parseFloat(argument);
                 }
             } catch (NumberFormatException ignored) {
             }
-
             triggerChannel.sendMessage(errorMessage.apply(argument, FORMAT.format(defaultValue))).queue();
             triggerChannel.sendTyping().queue();
         }
-
         return defaultValue;
+    }
+
+    private static class UncheckedException extends RuntimeException {
+
+        public UncheckedException(Throwable cause) {
+            super(cause);
+        }
     }
 }
