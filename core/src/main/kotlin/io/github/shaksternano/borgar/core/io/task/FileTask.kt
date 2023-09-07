5905e192bfa9bebc9abc55f4a05efd6a589e89c1
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-09-07 13:07:37 +0100

    feat: create file task interface

---
+++ core/src/main/kotlin/io/github/shaksternano/borgar/core/io/task/FileTask.kt
@@ -0,0 +1,34 @@
+package io.github.shaksternano.borgar.core.io.task
+
+import io.github.shaksternano.borgar.core.io.DataSource
+
+interface FileTask {
+
+    val requireInput: Boolean
+
+    suspend fun run(input: List<DataSource>): List<DataSource>
+
+    infix fun then(after: FileTask): FileTask {
+        if (!after.requireInput) {
+            throw IllegalArgumentException("The task after this one must require input")
+        }
+        return object : FileTask {
+            override val requireInput: Boolean = this@FileTask.requireInput
+
+            override suspend fun run(input: List<DataSource>): List<DataSource> {
+                val firstOutput = this@FileTask.run(input)
+                if (firstOutput.isEmpty()) {
+                    throw IllegalStateException("The first task must output at least one file")
+                }
+                return after.run(firstOutput)
+            }
+
+            override suspend fun cleanup() {
+                this@FileTask.cleanup()
+                after.cleanup()
+            }
+        }
+    }
+
+    suspend fun cleanup() = Unit
+}
