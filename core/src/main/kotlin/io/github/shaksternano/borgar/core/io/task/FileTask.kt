c505a5c42b1688dfb95acd2506b3d34090e87667
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-09-21 12:04:37 +0100

    refactor: change how files are deleted

--- core/src/main/kotlin/io/github/shaksternano/borgar/core/io/task/FileTask.kt
+++ core/src/main/kotlin/io/github/shaksternano/borgar/core/io/task/FileTask.kt
@@ -1,6 +1,10 @@
 package io.github.shaksternano.borgar.core.io.task
 
 import io.github.shaksternano.borgar.core.io.DataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.nio.file.Path
+import kotlin.io.path.deleteIfExists
 
 interface FileTask {
 
@@ -28,4 +32,18 @@ interface FileTask {
     }
 
     suspend fun cleanup() = Unit
+}
+
+abstract class BaseFileTask : FileTask {
+
+    protected val outputs: MutableList<Path> = mutableListOf()
+
+    override suspend fun cleanup() {
+        withContext(Dispatchers.IO) {
+            outputs.forEach {
+                it.deleteIfExists()
+            }
+        }
+        outputs.clear()
+    }
 }
--- core/src/main/kotlin/io/github/shaksternano/borgar/core/io/task/MediaProcessingTask.kt
+++ core/src/main/kotlin/io/github/shaksternano/borgar/core/io/task/MediaProcessingTask.kt
@@ -3,46 +3,29 @@ package io.github.shaksternano.borgar.core.io.task
 import io.github.shaksternano.borgar.core.io.DataSource
 import io.github.shaksternano.borgar.core.media.MediaProcessConfig
 import io.github.shaksternano.borgar.core.media.processMedia
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.nio.file.Path
-import kotlin.io.path.deleteIfExists
 
-abstract class MediaProcessingTask(
+class MediaProcessingTask(
     private val maxFileSize: Long,
-) : FileTask {
+    private val config: MediaProcessConfig,
+) : BaseFileTask() {
 
     override val requireInput: Boolean = true
-    protected abstract val config: MediaProcessConfig
-    private val filesToDelete: MutableList<Path> = mutableListOf()
 
-    override suspend fun run(input: List<DataSource>): List<DataSource> {
-        return input.map {
-            val path = it.getOrWriteFile()
-            if (!it.fromFile) {
-                filesToDelete.add(path)
-            }
-            val output = processMedia(path, config, maxFileSize)
-            return listOf(DataSource.fromFile(output.name, output.path))
-        }
+    override suspend fun run(input: List<DataSource>): List<DataSource> = input.map {
+        val path = it.getOrWriteFile().path
+        val output = processMedia(path, config, maxFileSize)
+        outputs.add(output.path)
+        output
     }
 
     override fun then(after: FileTask): FileTask {
         return if (after is MediaProcessingTask) {
-            object : MediaProcessingTask(maxFileSize) {
-                override val config: MediaProcessConfig = this@MediaProcessingTask.config then after.config
-            }
+            MediaProcessingTask(
+                maxFileSize,
+                this@MediaProcessingTask.config then after.config,
+            )
         } else {
             super.then(after)
         }
-    }
-
-    override suspend fun cleanup() {
-        withContext(Dispatchers.IO) {
-            filesToDelete.forEach {
-                it.deleteIfExists()
-            }
-        }
-        filesToDelete.clear()
     }
 }
