165a918957edfa8fa33c9761b4d6b0650095ed8b
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-09-22 01:11:23 +0100

    build: add logging dependency to `discord` module

--- discord/build.gradle.kts
+++ discord/build.gradle.kts
@@ -1,6 +1,7 @@
 val jdaVersion: String by project
 val jdaKtxVersion: String by project
 val discordWebhooksVersion: String by project
+val log4j2Version: String by project
 
 dependencies {
     implementation(project(":core"))
@@ -12,6 +13,8 @@ dependencies {
     implementation("com.github.minndevelopment:jda-ktx:${jdaKtxVersion}")
     @Suppress
     implementation("club.minnced:discord-webhooks:$discordWebhooksVersion")
+
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4j2Version")
 
     testImplementation(kotlin("test"))
 }
