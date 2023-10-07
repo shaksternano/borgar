b77c657a1374811011e39e7c69f8ca9d96b9e794
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-10-07 22:37:18 +0100

    feat: reimplement `ConstantFrameDurationMediaReader`

---
+++ core/src/main/kotlin/io/github/shaksternano/borgar/core/media/reader/ConstantFrameDurationMediaReader.kt
@@ -0,0 +1,56 @@
+package io.github.shaksternano.borgar.core.media.reader
+
+import io.github.shaksternano.borgar.core.media.VideoFrame
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.time.Duration
+
+class ConstantFrameDurationMediaReader<T : VideoFrame<*>>(
+    private val reader: MediaReader<T>,
+    override val frameDuration: Duration,
+    totalDuration: Duration,
+) : BaseMediaReader<T>() {
+
+    constructor(reader: MediaReader<T>, frameDuration: Duration) :
+        this(reader, frameDuration, reader.duration)
+
+    override val frameCount: Int = ceil(totalDuration / frameDuration).toInt()
+    override val frameRate: Double = 1000.0 / frameDuration.inWholeMilliseconds
+    override val duration: Duration = frameDuration * frameCount
+    override val audioChannels: Int = reader.audioChannels
+    override val audioSampleRate: Int = reader.audioSampleRate
+    override val audioBitrate: Int = reader.audioBitrate
+    override val width: Int = reader.width
+    override val height: Int = reader.height
+    override val loopCount: Int = reader.loopCount
+
+    @Suppress("UNCHECKED_CAST")
+    override suspend fun readFrame(timestamp: Duration): T {
+        val frameNumber = floor(timestamp / frameDuration)
+        val newTimestamp = (frameDuration * frameNumber)
+        return reader.readFrame(timestamp).copy(
+            duration = frameDuration,
+            timestamp = newTimestamp,
+        ) as T
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun asFlow(): Flow<T> = flow {
+        var currentTimestamp = Duration.ZERO
+        while (currentTimestamp < duration) {
+            val frame = reader.readFrame(currentTimestamp).copy(
+                duration = frameDuration,
+                timestamp = currentTimestamp,
+            ) as T
+            emit(frame)
+            currentTimestamp += frameDuration
+        }
+    }
+
+    override fun createReversed(): MediaReader<T> =
+        ConstantFrameDurationMediaReader(reader.reversed, frameDuration, duration)
+
+    override suspend fun close() = reader.close()
+}
