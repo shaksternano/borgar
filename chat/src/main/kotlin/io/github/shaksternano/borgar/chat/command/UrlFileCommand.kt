fc8b6c592ba40b906cc5871795680cac59d13136
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2024-01-16 22:24:00 +0000

    refactor: explicitly declare command type

--- chat/src/main/kotlin/io/github/shaksternano/borgar/chat/command/UrlFileCommand.kt
+++ chat/src/main/kotlin/io/github/shaksternano/borgar/chat/command/UrlFileCommand.kt
@@ -15,13 +15,13 @@ class UrlFileCommand(
     private val task: FileTask = UrlFileTask(url)
 
     companion object {
-        val HAEMA = UrlFileCommand(
+        val HAEMA: Command = UrlFileCommand(
             name = "haema",
             description = "https://modrinth.com/mod/haema",
             url = "https://media.discordapp.net/attachments/964551969509347331/1134818935829712987/YOU_SHOULD_DOWNLOAD_HAEMA_NOW.gif",
         )
 
-        val TULIN = UrlFileCommand(
+        val TULIN: Command = UrlFileCommand(
             name = "tulin",
             description = "The best character in The Legend of Zelda: Breath of the Wild.",
             url = "https://media.discordapp.net/attachments/964551969509347331/1119756264633798729/BotW_Tulin_Model.gif",
