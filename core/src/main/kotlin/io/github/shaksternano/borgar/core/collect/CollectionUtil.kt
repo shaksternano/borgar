802e45157d72e221ce0318e363f9eeea985ba6e2
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2024-02-05 17:09:15 +0000

    refactor: don't use async for collections with 1 or fewer elements

--- core/src/main/kotlin/io/github/shaksternano/borgar/core/collect/CollectionUtil.kt
+++ core/src/main/kotlin/io/github/shaksternano/borgar/core/collect/CollectionUtil.kt
@@ -7,12 +7,21 @@ import kotlinx.coroutines.coroutineScope
 fun <T> MutableCollection<in T>.addAll(vararg elements: T): Boolean =
     addAll(elements)
 
-suspend fun <T, R> Iterable<T>.parallelMap(transform: suspend (T) -> R): List<R> = coroutineScope {
-    map { async { transform(it) } }.awaitAll()
-}
+suspend fun <T, R> Iterable<T>.parallelMap(transform: suspend (T) -> R): List<R> =
+    if (this is Collection && size <= 1) {
+        map { transform(it) }
+    } else {
+        coroutineScope {
+            map { async { transform(it) } }.awaitAll()
+        }
+    }
 
 suspend fun <T> Iterable<T>.parallelForEach(action: suspend (T) -> Unit) {
-    parallelMap(action)
+    if (this is Collection && size <= 1) {
+        forEach { action(it) }
+    } else {
+        parallelMap(action)
+    }
 }
 
 fun <K, V> MutableMap<K, V>.putAllKeys(keys: Iterable<K>, value: V) = keys.forEach {
