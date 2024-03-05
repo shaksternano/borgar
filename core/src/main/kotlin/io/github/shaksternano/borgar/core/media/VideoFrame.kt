ce4ac815f81590bfb1fa9a53c3781e8444d4121b
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2024-03-05 21:26:06 +0000

    refactor: remove unused interface

--- core/src/main/kotlin/io/github/shaksternano/borgar/core/media/VideoFrame.kt
+++ core/src/main/kotlin/io/github/shaksternano/borgar/core/media/VideoFrame.kt
@@ -12,35 +12,3 @@ data class VideoFrame<T>(
 
 typealias ImageFrame = VideoFrame<BufferedImage>
 typealias AudioFrame = VideoFrame<Frame>
-
-interface VideoFrameOld<T, V : VideoFrameOld<T, V>> {
-
-    /**
-     * The content of the frame.
-     *
-     * @return The content of the frame.
-     */
-    val content: T
-
-    /**
-     * The duration of the frame in microseconds.
-     *
-     * @return The duration of the frame in microseconds.
-     */
-    val duration: Double
-
-    /**
-     * The timestamp of the frame in microseconds.
-     *
-     * @return The timestamp of the frame in microseconds.
-     */
-    val timestamp: Long
-
-    fun transform(newContent: T): V
-
-    fun transform(speedMultiplier: Float): V
-
-    fun transform(newContent: T, speedMultiplier: Float): V
-
-    fun transform(duration: Double, timestamp: Long): V
-}
