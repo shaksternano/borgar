4f6748055cf21c87cb3310aa2935d44ced9a7b58
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2024-03-27 11:38:28 +0000

    refactor: remove unnecessary anonymous class

--- messaging/src/main/kotlin/io/github/shaksternano/borgar/messaging/event/MessageCommandEvent.kt
+++ messaging/src/main/kotlin/io/github/shaksternano/borgar/messaging/event/MessageCommandEvent.kt
@@ -5,7 +5,6 @@ import io.github.shaksternano.borgar.messaging.BotManager
 import io.github.shaksternano.borgar.messaging.command.CommandArguments
 import io.github.shaksternano.borgar.messaging.command.CommandMessageIntersection
 import io.github.shaksternano.borgar.messaging.command.CommandResponse
-import io.github.shaksternano.borgar.messaging.command.getDefaultStringOrEmpty
 import io.github.shaksternano.borgar.messaging.entity.*
 import io.github.shaksternano.borgar.messaging.entity.channel.MessageChannel
 import kotlinx.coroutines.flow.Flow
@@ -46,25 +45,5 @@ data class MessageCommandEvent(
     }
 
     override fun asMessageIntersection(arguments: CommandArguments): CommandMessageIntersection =
-        object : CommandMessageIntersection {
-            override val manager: BotManager = this@MessageCommandEvent.manager
-            override val id: String = this@MessageCommandEvent.id
-            override val authorId: String = this@MessageCommandEvent.authorId
-            override val content: String = arguments.getDefaultStringOrEmpty()
-            override val attachments: List<Attachment> = event.message.attachments
-            override val embeds: List<MessageEmbed> = event.message.embeds
-            override val customEmojis: Flow<CustomEmoji> = event.message.customEmojis
-            override val stickers: Flow<Sticker> = event.message.stickers
-            override val referencedMessages: Flow<Message> = this@MessageCommandEvent.referencedMessages
-
-            override suspend fun getAuthor(): User = this@MessageCommandEvent.getAuthor()
-
-            override suspend fun getAuthorMember(): Member? = this@MessageCommandEvent.getAuthorMember()
-
-            override suspend fun getChannel(): MessageChannel = this@MessageCommandEvent.getChannel()
-
-            override suspend fun getGuild(): Guild? = this@MessageCommandEvent.getGuild()
-
-            override suspend fun getGroup(): Group? = this@MessageCommandEvent.getGroup()
-        }
+        event.message
 }
