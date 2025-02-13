68020288b9c528e3d8f137da97c3df9f8c6e7700
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-02-13 18:46:29 +0000

    fix: don't include filename in revolt emoji url

--- revolt/src/main/kotlin/io/github/shaksternano/borgar/revolt/entity/RevoltCustomEmoji.kt
+++ revolt/src/main/kotlin/io/github/shaksternano/borgar/revolt/entity/RevoltCustomEmoji.kt
@@ -1,12 +1,8 @@
 package io.github.shaksternano.borgar.revolt.entity
 
-import io.github.shaksternano.borgar.core.io.head
-import io.github.shaksternano.borgar.core.io.useHttpClient
-import io.github.shaksternano.borgar.core.util.encodeUrl
 import io.github.shaksternano.borgar.messaging.entity.BaseEntity
 import io.github.shaksternano.borgar.messaging.entity.CustomEmoji
 import io.github.shaksternano.borgar.revolt.RevoltManager
-import io.ktor.http.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -28,27 +24,12 @@ data class RevoltEmojiResponse(
     val name: String,
     val animated: Boolean = false,
 ) {
-    suspend fun convert(manager: RevoltManager): RevoltCustomEmoji {
-        val url = "${manager.cdnUrl}/emojis/$id"
-        val extension = runCatching {
-            useHttpClient { client ->
-                client.head(url)
-                    .contentType()
-                    ?.contentSubtype
-                    ?.lowercase()
-            }
-        }.getOrNull().let {
-            if (it.isNullOrBlank()) {
-                if (animated) "gif" else "png"
-            } else {
-                it
-            }
-        }
+    fun convert(manager: RevoltManager): RevoltCustomEmoji {
         return RevoltCustomEmoji(
             manager = manager,
             id = id,
             name = name,
-            imageUrl = "$url/${name.encodeUrl()}.$extension"
+            imageUrl = "${manager.cdnUrl}/emojis/$id"
         )
     }
 }
