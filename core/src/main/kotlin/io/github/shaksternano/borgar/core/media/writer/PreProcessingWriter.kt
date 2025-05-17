fd1400a83ce9078b8bcb8a79ca0ff5e59e25c094
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-05-18 00:34:32 +0100

    perf: `PreProcessingWriter` writes frames asynchronously

--- core/src/main/kotlin/io/github/shaksternano/borgar/core/media/writer/PreProcessingWriter.kt
+++ core/src/main/kotlin/io/github/shaksternano/borgar/core/media/writer/PreProcessingWriter.kt
@@ -1,55 +1,85 @@
 package io.github.shaksternano.borgar.core.media.writer
 
-import io.github.shaksternano.borgar.core.io.SuspendCloseable
 import io.github.shaksternano.borgar.core.io.closeAll
 import io.github.shaksternano.borgar.core.media.ImageFrame
-import io.github.shaksternano.gifcodec.ChannelOutputAsyncExecutor
-import io.github.shaksternano.gifcodec.forEach
-import io.github.shaksternano.gifcodec.forEachCurrent
+import io.github.shaksternano.gifcodec.AsyncExecutor
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import kotlinx.io.IOException
 import java.awt.image.BufferedImage
+import kotlin.concurrent.atomics.AtomicReference
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
 import kotlin.coroutines.EmptyCoroutineContext
 
 class PreProcessingWriter(
     private val writer: MediaWriter,
     maxConcurrency: Int,
     scope: CoroutineScope = CoroutineScope(EmptyCoroutineContext),
-    preProcessImage: (BufferedImage) -> BufferedImage,
+    private val preProcessImage: (BufferedImage) -> BufferedImage,
 ): MediaWriter by writer {
 
-    private val preProcessExecutor: ChannelOutputAsyncExecutor<ImageFrame, ImageFrame> = ChannelOutputAsyncExecutor(
+    @OptIn(ExperimentalAtomicApi::class)
+    private val throwableReference: AtomicReference<Throwable?> = AtomicReference(null)
+
+    private val preProcessExecutor: AsyncExecutor<ImageFrame, ImageFrame> = AsyncExecutor(
         maxConcurrency = maxConcurrency,
         scope = scope,
-        task = { frame ->
-            frame.copy(content = preProcessImage(frame.content))
+        task = ::processFrame,
+        onOutput = {
+            writeFrame(it)
         },
     )
 
-    override suspend fun writeImageFrame(frame: ImageFrame) = coroutineScope {
-        val preProcessJob = launch {
-            preProcessExecutor.submit(frame)
+    override suspend fun writeImageFrame(frame: ImageFrame) {
+        @OptIn(ExperimentalAtomicApi::class)
+        val throwable = throwableReference.load()
+        if (throwable != null) {
+            throw createException(throwable)
         }
-        while (preProcessJob.isActive) {
-            preProcessExecutor.output.forEachCurrent {
-                writer.writeImageFrame(it.getOrThrow())
-            }
-        }
+        preProcessExecutor.submit(frame)
     }
 
-    override suspend fun close() = coroutineScope {
-        val writeJob = launch {
-            preProcessExecutor.output.forEach {
-                writer.writeImageFrame(it.getOrThrow())
+    private fun processFrame(frame: ImageFrame): ImageFrame {
+        return frame.copy(content = preProcessImage(frame.content))
+    }
+
+    private suspend fun writeFrame(frameResult: Result<ImageFrame>) {
+        val error = frameResult.exceptionOrNull()
+        if (error != null) {
+            @OptIn(ExperimentalAtomicApi::class)
+            throwableReference.compareAndSet(null, error)
+            return
+        }
+        val output = frameResult.getOrThrow()
+        writer.writeImageFrame(output)
+    }
+
+    private fun createException(cause: Throwable): IOException {
+        return IOException("Error pre-processing frame", cause)
+    }
+
+    override suspend fun close() {
+        var closeThrowable: Throwable? = null
+        try {
+            closeAll(
+                {
+                    preProcessExecutor.close()
+                },
+                writer,
+            )
+        } catch (t: Throwable) {
+            closeThrowable = t
+            throw t
+        } finally {
+            @OptIn(ExperimentalAtomicApi::class)
+            val throwable = throwableReference.load()
+            if (throwable != null) {
+                val exception = createException(throwable)
+                if (closeThrowable == null) {
+                    throw exception
+                } else {
+                    closeThrowable.addSuppressed(exception)
+                }
             }
         }
-        closeAll(
-            SuspendCloseable {
-                preProcessExecutor.close()
-                writeJob.join()
-            },
-            writer,
-        )
     }
 }
